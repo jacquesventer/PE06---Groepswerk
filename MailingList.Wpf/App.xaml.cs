19acb8f415d00898a37b372f3d716b8dcfbcc3ed
jacquesventer <jacquesventer@gmail.com>
2018-05-16 19:10:17 +0200

    Aanpassing classes

--- MailingList.Wpf/App.xaml.cs
+++ MailingList.Wpf/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace MailingList.Lib
+namespace MailingList.Wpf
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- MailingList.Wpf/MainWindow.xaml.cs
+++ MailingList.Wpf/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MailingList.Lib
+namespace MailingList.Wpf
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
